d5ea4b5ea4bc2415264d05edd23e4fb76d9d3988
Nguyen Hoang Yen <nguyenhoangyenvn@gmail.com>
2016-06-08 14:59:44 +0700

    Finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,9 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
-  	def configure_permitted_parameters
-  		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :title)}
-  		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, 
-  			:current_password, :title, :description, :itunes, :stitcher, :podbay)}
-  	end
 
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :title, :thumbnail)}
+  	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :title, :thumbnail, :description, :itunes, :stitcher, :podbay)}
+  end
 end
